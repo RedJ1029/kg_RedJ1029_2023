815693642048afc0d60a7fc9fb27b825e995577f
Justin Di Savino <jrd329@cornell.edu>
2021-05-01 15:06:44 -0400

    Added strOfInt function: converts an int in 0..9 to its phonetic equivalent.

---
+++ main.js
@@ -0,0 +1,23 @@
+function strOfInt(x) {
+  if (x === 0) {
+    return "Zero"
+  } else if (x === 1) {
+    return "One"
+  } else if (x === 2) {
+    return "Two"
+  } else if (x === 3) {
+    return "Three"
+  } else if (x === 4) {
+    return "Four"
+  } else if (x === 5) {
+    return "Five"
+  } else if (x === 6) {
+    return "Six"
+  } else if (x === 7) {
+    return "Seven"
+  } else if (x === 8) {
+    return "Eight"
+  } else {
+    return "Nine"
+  }
+}
